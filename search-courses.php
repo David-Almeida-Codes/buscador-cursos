373db99b13dc9c6f964a5963622ca43da3d5ec24
David Almeida <davidsouzacontato@gmail.com>
2020-01-04 02:55:41 -0300

    creating something

---
+++ search-courses.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once 'src/SearchEngine.php';
+
+use Dac\SearchEngine\SearchEngine;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$searchEngine = new SearchEngine($client, $crawler);
+$courses = $searchEngine->search('/cursos-online-programacao/php');
+
+foreach($courses as $course){
+    echo $course . PHP_EOL;
+}
+
+
+
---
+++ src/SearchEngine.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Dac\SearchEngine;
+
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class SearchEngine
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function search(string $url): array
+    {
+
+        $response = $this->httpClient->request('GET', $url);
+
+        $html = $response->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $coursesElements = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($coursesElements as $element){
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
+
+
+}
